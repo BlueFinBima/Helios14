14519d5d1500813577946cb43c8084d09623ce50
wheelchock <6605805+Todd1215@users.noreply.github.com>
2020-06-01 10:16:04 -0400

    Update FalconTypes.cs
    Removed OpenFalcon and AlliedForces

--- HeliosFalcon/Interfaces/FalconTypes.cs
+++ HeliosFalcon/Interfaces/FalconTypes.cs
@@ -19,10 +19,6 @@ namespace GadrocsWorkshop.Helios.Interfaces.Falcon
 {
     public enum FalconTypes
     {
-        [Description("Open Falcon 4.7")]
-        OpenFalcon,
-        [Description("Allied Forces")]
-        AlliedForces,
         [Description("BMS 4.34")]
         BMS
     }
